1e7d8cc877de433195a01cf52d3f4a48ba802190
Daniel Ferreira <dcferreira@users.noreply.github.com>
2018-02-21 12:07:52 +0100

    added some informative messages

--- check.sh
+++ check.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+printf "Checking for updated image...\n"
 docker pull dcferreira/nta-meta-analysis-verification
+printf "\n\n--------------------------------\nResults of paper verifier:\n"
 docker run --mount type=bind,source=$1,target=/paper dcferreira/nta-meta-analysis-verification /paper
