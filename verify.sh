24f0144511e0e3f38ab119f9177916a84e397844
Daniel Ferreira <dcferreira@users.noreply.github.com>
2018-05-09 16:05:18 +0200

    verification script now supports MAJOR.MINOR.PATCH versions (ignores PATCH number)

--- verify.sh
+++ verify.sh
@@ -4,6 +4,9 @@ ROOTDIR=`git rev-parse --show-toplevel`
 SPEC=$2
 
 VERSION=`cat $FILENAME | python -c "from __future__ import print_function; import sys, json; print(json.load(sys.stdin)['version'])" | sed s/v/r/`
+VERSION=`(echo "from __future__ import print_function" ;
+          echo "v = '$VERSION'.split('.')" ;
+          echo "print('.'.join(v[:2]) if len(v) > 1 else v[0])") | python`
 
 if [ ! -d "$ROOTDIR/.$VERSION" ]
 then
@@ -11,6 +14,6 @@ then
     then
         read -p "What is the path for the local git specification repo?" SPEC
     fi
-    (cd $SPEC && git worktree prune && git worktree add --force $ROOTDIR/.$VERSION origin/$VERSION > /dev/null 2>&1 )
+    (cd $SPEC && git worktree prune && git worktree add --force $ROOTDIR/.$VERSION $VERSION ) #origin/$VERSION )#> /dev/null 2>&1 )
 fi
 $ROOTDIR/.$VERSION/verify.sh $FILENAME
